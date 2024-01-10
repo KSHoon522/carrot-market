d67121e77dec41cce6b4c9f4d9d277f1b68ac49a
Sang Hoon Kim <KSHoon522@gamil.com>
2024-01-10 09:31:30 +0900

    Add Stream

--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -69,19 +69,6 @@ const StreamDetail: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="fixed w-full mx-auto max-w-md bottom-2 inset-x-0">
-        <div className="flex relative items-center">
-          <input
-            type="text"
-            className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none focus:border-orange-500 pr-12"
-          />
-          <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
-            <button className="flex items-center bg-orange-500 focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 rounded-full px-3 text-sm text-white hover:bg-orange-600">
-              &rarr;
-            </button>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
