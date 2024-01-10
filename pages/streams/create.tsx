f5c76962e5e6ee13026376c863353096f67b5971
Sang Hoon Kim <KSHoon522@gamil.com>
2024-01-10 15:27:42 +0900

    End tailwindcss

--- pages/streams/create.tsx
+++ pages/streams/create.tsx
@@ -1,63 +1,26 @@
 import type { NextPage } from "next";
+import Button from "../../components/button";
+import Input from "../../components/input";
+import Layout from "../../components/layout";
+import TextArea from "../../components/textarea";
 
 const Create: NextPage = () => {
   return (
-    <div className="px-4 space-y-5 py-16">
-      <div>
-        <label
-          className="mb-1 block text-sm font-medium text-gray-700"
-          htmlFor="name"
-        >
-          Name
-        </label>
-        <div className="rounded-md relative flex  items-center shadow-sm">
-          <input
-            id="name"
-            type="email"
-            className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-            required
-          />
-        </div>
-      </div>
-      <div>
-        <label
-          htmlFor="price"
-          className="mb-1 text-sm font-medium text-gray-700"
-        >
-          Price
-        </label>
-        <div className="rounded-md shadow-sm relative flex items-center">
-          <div className="absolute left-0 pl-3 flex items-center justify-center">
-            <span className="text-gray-500 text-sm pointer-events-none">$</span>
-          </div>
-          <input
-            id="price"
-            type="text"
-            className="appearance-none pl-7 w-full px-3 py-2.5 border border-gray-300 rounded-md shaodw-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-            placeholder="0.00"
-          />
-          <div className="absolute right-0 pr-3 flex items-center">
-            <span className="text-gray-500 pointer-events-none">USD</span>
-          </div>
-        </div>
-      </div>
-      <div>
-        <label
-          className="mb-1 text-sm font-medium text-gray-700"
-          htmlFor="description"
-        >
-          Description
-        </label>
-        <textarea
-          className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500 resize-none"
-          id="description"
-          rows={4}
+    <Layout canGoBack title="Go Live">
+      <form className=" space-y-4 py-10 px-4">
+        <Input required label="Name" name="name" type="text" />
+        <Input
+          required
+          label="Price"
+          placeholder="0.00"
+          name="price"
+          type="text"
+          kind="price"
         />
-      </div>
-      <button className="mt-5 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border-transparent rounded-md shadow-sm font-medium text-sm focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
-        Go live
-      </button>
-    </div>
+        <TextArea name="description" label="Description" />
+        <Button text="Go live" />
+      </form>
+    </Layout>
   );
 };
 
--- pages/streams/index.tsx
+++ pages/streams/index.tsx
@@ -1,33 +1,38 @@
-import { NextPage } from "next";
+import type { NextPage } from "next";
+import Link from "next/link";
+import FloatingButton from "../../components/floating-button";
+import Layout from "../../components/layout";
 
-const Stream: NextPage = () => {
+const Live: NextPage = () => {
   return (
-    <div className="py-10  divide-y-2 space-y-4">
-      {[1, 2, 3, 4, 5].map((_, i) => (
-        <div className="pt-4 px-4" key={i}>
-          <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
-          <h3 className=" text-gray-700 text-lg mt-2">Let&apos;s try DIY</h3>
-        </div>
-      ))}
-      <button className="fixed bottom-24 right-5 bg-orange-400 rounded-full p-4 text-white shadow-xl hover:bg-orange-500 border-0 transition-colors">
-        <svg
-          className="h-6 w-6"
-          fill="none"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z"
-          />
-        </svg>
-      </button>
-    </div>
+    <Layout hasTabBar title="라이브">
+      <div className=" divide-y-[1px] space-y-4">
+        {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <Link key={i} href={`/streams/${i}`} className="pt-4 block  px-4">
+            <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
+            <h1 className="text-2xl mt-2 font-bold text-gray-900">
+              Galaxy S50
+            </h1>
+          </Link>
+        ))}
+        <FloatingButton href="/streams/create">
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
+            ></path>
+          </svg>
+        </FloatingButton>
+      </div>
+    </Layout>
   );
 };
-
-export default Stream;
+export default Live;
