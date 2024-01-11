72c0363d21802135490a582e3de81983ed17d8e6
Sang Hoon Kim <KSHoon522@gamil.com>
2024-01-11 17:19:25 +0900

    Errors / Extras

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,22 +1,37 @@
+import { useEffect } from "react";
 import { FieldErrors, useForm } from "react-hook-form";
 
 interface LoginForm {
   username: string;
   email: string;
   password: string;
+  errors?: string;
 }
 
 export default function Forms() {
-  const { register, watch, handleSubmit } = useForm<LoginForm>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    setError,
+    setValue,
+    reset,
+  } = useForm<LoginForm>({ mode: "onChange" });
   const onValid = (data: LoginForm) => {
     console.log("valid");
+    setError("errors", { message: "backend is offline" });
+    reset();
   };
   const onInvalid = (errors: FieldErrors) => {
     console.log(errors);
   };
-  console.log(watch());
+  console.log(watch("email"));
+  useEffect(() => {
+    setValue("username", "hello");
+  });
   return (
-    <form onSubmit={handleSubmit(onValid, onInvalid)}>
+    <form onSubmit={handleSubmit(onValid, onInvalid)} className="flex flex-col">
       <input
         {...register("username", {
           required: "Username is required",
@@ -26,16 +41,26 @@ export default function Forms() {
         placeholder="Username"
       />
       <input
-        {...register("email", { required: true })}
+        {...register("email", {
+          required: "Email is required",
+          validate: {
+            notGmail: (value) =>
+              !value.includes("@gmail.com") || "gmail is not allowed",
+          },
+        })}
         type="email"
         placeholder="Email"
       />
+      <span className={`${errors.email?.message ? "text-red-500" : ""}`}>
+        {errors.email?.message}
+      </span>
       <input
         {...register("password", { required: true })}
         type="password"
         placeholder="Password"
       />
       <input type="submit" value="create account" />
+      {errors.errors?.message}
     </form>
   );
 }
