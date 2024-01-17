01aec6a4baf68525e156844e18fa146f14e8a975
Sang Hoon Kim <KSHoon522@gamil.com>
2024-01-17 17:39:15 +0900

    Favorite products

--- pages/api/products/[id]/index.ts
+++ pages/api/products/[id]/index.ts
@@ -7,7 +7,10 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const { id } = req.query;
+  const {
+    query: { id },
+    session: { user },
+  } = req;
   const product = await client.product.findUnique({
     where: {
       id: +id!.toString(),
@@ -33,11 +36,22 @@ async function handler(
       },
     },
   });
-  console.log(relatedProducts);
+  const isLiked = Boolean(
+    await client.fav.findFirst({
+      where: {
+        productId: product?.id,
+        userId: user?.id,
+      },
+      select: {
+        id: true,
+      },
+    })
+  );
   res.json({
     ok: true,
     product,
     relatedProducts,
+    isLiked,
   });
 }
 
