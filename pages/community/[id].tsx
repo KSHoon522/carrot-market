ffaa8e69dd697c000c62059b3621783bb0ff4b55
Sang Hoon Kim <KSHoon522@gamil.com>
2024-01-18 16:42:30 +0900

    Reply / All posts

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -8,6 +8,7 @@ import Link from "next/link";
 import { useForm } from "react-hook-form";
 import useMutation from "@/libs/client/useMutation";
 import { cls } from "@/libs/client/utils";
+import { useEffect } from "react";
 
 interface AnswerWithUser extends Answer {
   user: User;
@@ -27,14 +28,26 @@ interface CommunityPostResponse {
   post: PostWithUser;
   isWondering: boolean;
 }
+interface AnswerForm {
+  answer: string;
+}
+
+interface AnswerResponse {
+  ok: boolean;
+  answer: Answer;
+}
 
 const CommunityPostDetail: NextPage = () => {
   const router = useRouter();
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm<AnswerForm>();
   const { data, mutate } = useSWR<CommunityPostResponse>(
     router.query.id ? `/api/posts/${router.query.id}` : null
   );
-  const [wonder] = useMutation(`/api/posts/${router.query.id}/wonder`);
+  const [wonder, { loading }] = useMutation(
+    `/api/posts/${router.query.id}/wonder`
+  );
+  const [sendAnswer, { data: answerData, loading: answerLoading }] =
+    useMutation<AnswerResponse>(`/api/posts/${router.query.id}/answers`);
   const onWonderClick = () => {
     if (!data) return;
     mutate(
@@ -53,8 +66,20 @@ const CommunityPostDetail: NextPage = () => {
       },
       false
     );
-    wonder({});
+    if (!loading) {
+      wonder({});
+    }
   };
+  const onValid = (form: AnswerForm) => {
+    if (answerLoading) return;
+    sendAnswer(form);
+  };
+  useEffect(() => {
+    if (answerData?.ok) {
+      reset();
+      mutate();
+    }
+  }, [answerData, reset, mutate]);
   return (
     <Layout canGoBack>
       <div>
@@ -129,27 +154,27 @@ const CommunityPostDetail: NextPage = () => {
               <div className="w-8 h-8 bg-slate-200 rounded-full" />
               <div>
                 <span className="text-sm block font-medium text-gray-700">
-                  {answer.user.name}
+                  {answer?.user?.name}
                 </span>
                 <span className="text-xs text-gray-500 block ">
-                  {answer.createdAt.toString()}
+                  {answer?.createdAt?.toString()}
                 </span>
-                <p className="text-gray-700 mt-2">{answer.answer}</p>
+                <p className="text-gray-700 mt-2">{answer?.answer}</p>
               </div>
             </div>
           ))}
         </div>
-        <div className="px-4">
+        <form onSubmit={handleSubmit(onValid)} className="px-4">
           <TextArea
-            register={register("reply", { required: true })}
+            register={register("answer", { required: true, minLength: 5 })}
             name="description"
             placeholder="Answer this question!"
             required
           />
           <button className="mt-2 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none ">
-            Reply
+            {answerLoading ? "Loading..." : "Reply"}
           </button>
-        </div>
+        </form>
       </div>
     </Layout>
   );
